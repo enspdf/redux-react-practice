de0a620770450646472e3ddbe21795d190860cf6
Sebastian Higuita <sebastianhc1995@hotmail.com>
2020-02-24 00:02:42 -0500

    finish search

--- src/components/search/PokemonResult.jsx
+++ src/components/search/PokemonResult.jsx
@@ -1,15 +1,19 @@
 import React from "react";
+import { useSelector } from "react-redux";
 
 const PokemonResult = () => {
+    const searcher = useSelector((state) => state.search);
+
     return (
         <div>
             <h3 className="text-white">Resultado: </h3>
-            <div className="text-warning">Buscando...</div>
-            <div className="text-success">
-                <img src="" alt="" />
-                <span>pikachu</span>
-            </div>
-            <span className="text-danger">Error</span>
+            {searcher.loading && <div className="text-warning">Buscando...</div>}
+            {searcher.pokemon.length >= 1 &&
+                <div className="text-success">
+                    <img src={searcher.pokemon[0].sprites.front_default} alt="Pokemon" />
+                    <span>{searcher.pokemon[0].name}</span>
+                </div>}
+            {searcher.error !== "" && <span className="text-danger">{searcher.error}</span>}
         </div>
     );
 };
